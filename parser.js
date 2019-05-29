7fb6522fa4d80526810db587522c4b424d72eff8
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-29 18:05:34 -0400

    after looping check to see that parens are even

--- parser.js
+++ parser.js
@@ -2,16 +2,20 @@ const { example1, example2, longExample } = require('./examples.js');
 
 const parser = (inputString) => {
   let compiles = true;
-  const strLength = inputString.length;
   let leftStack = [];
   let rightStack = [];
+
+  const strLength = inputString.length;
   console.log({ length: inputString.length});
+
   for( let i = 0; i < strLength; i++){
     const val = inputString[i];
-    console.log(
-      val,
-      val === '\(',
-      val === '\)');
+
+    // console.log(
+    //   val,
+    //   val === '\(',
+    //   val === '\)');
+
     if( val === '\(' ) {
       leftStack.push(val)
     } else if (val === '\)'){
@@ -27,7 +31,20 @@ const parser = (inputString) => {
       continue;
     }
   }
+  compiles =
+    (compiles === true && leftStack.length === rightStack.length)
+      ? true : false;
+
   console.log({compiles})
+  return compiles
 }
 
 parser(example1)
+parser(example2)
+parser(longExample)
+parser(longExample + '\(')
+parser(longExample + '\)')
+parser('\(' + longExample)
+parser('\(' + longExample)
+
+module.exports = { parser }
