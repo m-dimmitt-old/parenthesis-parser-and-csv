fba52678615dc2eee78bb731ba9f146463c6fbd1
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-29 18:42:03 -0400

    enhancement: handle lisp semicolin comments

--- examples.js
+++ examples.js
@@ -1,3 +1,25 @@
+
+const commentingExample = `
+;;;; Math Utilities
+
+;;; FIB computes the the Fibonacci function in the traditional
+;;; recursive way.
+
+(defun fib (n)
+  (check-type n integer)
+  ;; At this point we're sure we have an integer argument.
+  ;; Now we can get down to some serious computation.
+  (cond ((< n 0)
+         ;; Hey, this is just supposed to be a simple example.
+         ;; Did you really expect me to handle the general case?
+         (error "FIB got ~D as an argument." n))
+        ((< n 2) n)             ;fib[0]=0 and fib[1]=1
+        ;; The cheap cases didn't work.
+        ;; Nothing more to do but recurse.
+        (t (+ (fib (- n 1))     ;The traditional formula
+              (fib (- n 2)))))) ; is fib[n-1]+fib[n-2].
+`
+
 const example2 =`
   (defun afisare (i j g h)
   (print (aref x i j))
@@ -13,15 +35,15 @@ const example1 =`
 const longExample = `
 (setf x (make-array '(3 3):initial-contents'((1 2 3)(4 5 6)(7 8 9))))
   (write x)
-  
+
   (defun afisare (i j g h)
   (print (aref x i j))
   (print (aref x g h))
   )
-  
+
   (afisare 0 1 0 2)
-  
-  
+
+
   (setq myarray (make-array '(3 2 3)
      :initial-contents
      '(((a b c) (1 2 3))
@@ -33,21 +55,21 @@ const longExample = `
   (write myarray)
   (terpri)
   (write array2)
-  
-  
+
+
   (setf (get 'student 'Nume) 'Dan)
   (setf (get 'student 'Varsta) 21)
   (setf (get 'student 'An) 3)
   (setf (get 'student 'cnp) 1970506350036)
-  
+
   (terpri)
   (write (symbol-plist 'student))
-  
-  
+
+
   (defun averagenum (n1 n2 n3)
      (/ ( + n1 n2 n3) 3)
   )
   (write(averagenum 5 10 6))
 `
 
-module.exports = { longExample, example1, example2 }
+module.exports = { longExample, example1, example2, commentingExample }
--- parser.js
+++ parser.js
@@ -1,27 +1,31 @@
-const { example1, example2, longExample } = require('./examples.js');
+const { example1, example2, commentingExample, longExample } = require('./examples.js');
 
 const parser = (inputString) => {
   let compiles = true;
   let leftStack = [];
   let rightStack = [];
-
-  const strLength = inputString.length;
-
-  for( let i = 0; i < strLength; i++){
-    const val = inputString[i];
-    if( val === '\(' ) {
-      leftStack.push(val)
-    } else if (val === '\)'){
-      if(leftStack.length > rightStack.length){
-        rightStack.push(val)
+  const lines = inputString.split("\n");
+  for( let index1 = 0; index1 < lines.length; index1++){
+    if(compiles===false){break;};
+    const line = lines[index1];
+    for( let index2 = 0; index2 < line.length; index2++){
+      const val = line[index2];
+      if( val === ';' ) {
+        break;
+      } else if (val === '('){
+        leftStack.push(val);
+      } else if (val === ')'){
+        if(leftStack.length > rightStack.length){
+          rightStack.push(val);
+        }
+        else {
+          compiles=false;
+          break;
+        }
       }
       else {
-        compiles=false;
-        break;
+        continue;
       }
-    }
-    else {
-      continue;
     }
   }
   compiles =
@@ -31,7 +35,11 @@ const parser = (inputString) => {
   return compiles
 }
 
-// parser(example1)
+// console.log( parser( "(" + commentingExample))
+// console.log( parser( "; (" + commentingExample))
+// console.log( parser( commentingExample + "; (") )
+// console.log( parser( commentingExample + "; )") )
+
 // parser(example2)
 // parser(longExample)
 // parser(longExample + '\(')
