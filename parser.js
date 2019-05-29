36d99399ee553c54f39b789a25b2e943a242aef6
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-29 17:50:48 -0400

    check left and right parenthesis stack in a for loop

--- parser.js
+++ parser.js
@@ -1,16 +1,33 @@
 const { example1, example2, longExample } = require('./examples.js');
 
 const parser = (inputString) => {
+  let compiles = true;
   const strLength = inputString.length;
   let leftStack = [];
   let rightStack = [];
   console.log({ length: inputString.length});
   for( let i = 0; i < strLength; i++){
+    const val = inputString[i];
     console.log(
-      inputString[i],
-      inputString[i] === '\(',
-      inputString[i] === '\)');
+      val,
+      val === '\(',
+      val === '\)');
+    if( val === '\(' ) {
+      leftStack.push(val)
+    } else if (val === '\)'){
+      if(leftStack.length > rightStack.length){
+        rightStack.push(val)
+      }
+      else {
+        compiles=false;
+        break;
+      }
+    }
+    else {
+      continue;
+    }
   }
+  console.log({compiles})
 }
 
 parser(example1)
