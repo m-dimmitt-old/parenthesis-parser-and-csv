8532566564f94571c83d3c2c6bcece71295f171f
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-30 10:58:45 -0400

    sorter function added and remove old files.

--- csv_assignment/csv_parser.sh
+++ csv_assignment/csv_parser.sh
@@ -6,6 +6,7 @@ sortByFirstLastThenSortValueAndRemoveDups(){
 
 ## solution for small jobs
 smallSolution(){
+  rm -rf divided_by_company; ## clear out csv's jobs, if you want to save them move them to a stable folder.
   mkdir divided_by_company;
   csv_filename=$1;
   cd divided_by_company; awk -F, 'gsub(/"/, "", $3);{print > $3}' ../$1; cd ..;
@@ -13,11 +14,16 @@ smallSolution(){
 
 ## scalable solution
 scalableSolution(){
+  rm -rf divided_by_company; ## clear out csv's jobs, if you want to save them move them to a stable folder.
   csv_filename=$1;
   mkdir divided_by_company;
   cd divided_by_company; awk -F, 'gsub(/"/, "", $3);{print > $3};{close($3);}' ../$csv_filename; cd ..;
   cd divided_by_company; for f in * ; do (echo "$f" && awk "/${f}/ { print }" ../$csv_filename > $f); done && cd ..;
+}
+
+sorter(){
   cd divided_by_company; for f in * ; do (sortByFirstLastThenSortValueAndRemoveDups $f); done && cd ..;
 }
 
 scalableSolution info.csv
+sorter
