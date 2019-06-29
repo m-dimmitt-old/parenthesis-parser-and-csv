d99d107ff84d47273437a9a8143b393f261aaf59
MichaelDimmitt <michaelgdimmitt@gmail.com>
2019-06-29 11:53:33 -0400

    optimize and improve naming.

--- csv_assignment/csv_parser.sh
+++ csv_assignment/csv_parser.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
-sortByFirstLastThenSortValueAndRemoveDups(){
+
+splitCsvByinsuranceName(){
+  awk -F, 'gsub(/"/, "", $3);{print > $3};{close($3);}' ../$csv_filename
+}
+
+populateChildFileWithData(){
+  echo "$1" && awk "/${1}/ { print }" ../$csv_filename > $1
+}
+
+sortByFirstLastThenSortByValueAndRemoveDups(){
   csv_filename=$1
   path=$(pwd)
   file_path=$path/$csv_filename
   sort -t, -r -k4,4 $file_path | sort -t, -u -k2,2 -k1,1 > $(echo $file_path)_sorted.csv
 }
 
-## solution for small jobs
-smallSolution(){
-  rm -rf divided_by_company; ## clear out csv's jobs, if you want to save them move them to a stable folder.
-  mkdir divided_by_company;
-  csv_filename=$1;
-  cd divided_by_company; awk -F, 'gsub(/"/, "", $3);{print > $3}' ../$1; cd ..;
+populateAndSort(){
+    populateChildFileWithData $1; 
+    sortByFirstLastThenSortByValueAndRemoveDups $1;
 }
 
 ## scalable solution
@@ -19,13 +25,8 @@ scalableSolution(){
   rm -rf divided_by_company; ## clear out csv's jobs, if you want to save them move them to a stable folder.
   csv_filename=$1;
   mkdir divided_by_company;
-  cd divided_by_company; awk -F, 'gsub(/"/, "", $3);{print > $3};{close($3);}' ../$csv_filename; cd ..;
-  cd divided_by_company; for f in * ; do (echo "$f" && awk "/${f}/ { print }" ../$csv_filename > $f); done && cd ..;
-}
-
-sorter(){
-  cd divided_by_company; for f in * ; do (sortByFirstLastThenSortValueAndRemoveDups $f); done && cd ..;
+  cd divided_by_company; splitCsvByinsuranceName $csv_filename; cd ..;
+  for f in divided_by_company/* ; do (populateAndSort $f); done;
 }
 
 scalableSolution info.csv
-sorter
